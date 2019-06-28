60104bc73be215e7f68ee4d694b7720b4dee1c5f
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-28 12:49:05 +0200

    add SearchBar component and add Bootstrap to project

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
   render() {
     return(
       <div>
         <Header />
+        <SearchBar />
       </div>
     );
   };
---
+++ src/components/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class SearchBar extends React.Component {
+    render() {
+        return(
+            <div className="container">
+                <form>
+                    <div className="input-group mb-3 input-group-lg">
+                        <input type="text" className="form-control" placeholder="Search GitHub User" aria-label="Search GitHub User" aria-describedby="button-addon2"/>
+                        <div className="input-group-append">
+                            <button type="button" className="btn btn-outline-success btn-lg">Search</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    };
+}
+
+export default SearchBar;
