f837b11c8fd01b9b01cae42a8c753c03a284e578
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-30 17:38:46 +0200

    add SearchBar.css

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {inputValue, getUser} from '../actions';
+import './styles/SearchBar.css';
 
 class SearchBar extends React.Component {
     
@@ -8,7 +9,7 @@ class SearchBar extends React.Component {
         const { iValue } = this.props;
         
         return(
-            <div className="container">
+            <div className="container search-container">
                 <form onSubmit={(e)=>this.props.getUser(e, iValue)}>
                     <div className="input-group mb-3 input-group-lg">
                         <input onChange={(e)=>this.props.inputValue(e.target.value)} type="text" className="form-control" placeholder="Search GitHub User" aria-label="Search GitHub User" aria-describedby="button-addon2"/>
