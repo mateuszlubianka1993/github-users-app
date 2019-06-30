2fb704d944aa756facb5aa2725ac0e35b6bc67c1
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-30 09:58:59 +0200

    add getUser action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,20 @@
+import githubApi from '../api/githubApi';
+
 export const inputValue = (data) => {
     return {
         type: 'INPUT_VALUE',
         payload: data
     }
 };
+
+export const getUser = async (e, iValue) => {
+    e.preventDefault();
+
+    const response = await githubApi.get(`/users/${iValue}`);
+    console.log(response);
+    return {
+        type: 'FETCH_USER', 
+        payload: response.data 
+    }
+}
 
