cb6c5076bf8888ae0df5b3a7d2c067d88a6aa7b0
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-30 21:39:56 +0200

    add UserDisplay.css

--- src/components/UserDisplay.js
+++ src/components/UserDisplay.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import './styles/UserDisplay.css';
 
 class UserDisplay extends React.Component {
 
@@ -7,16 +8,18 @@ class UserDisplay extends React.Component {
         const { user } = this.props;
         console.log(user)
         return(
-            <div className="container">
-                <div className="card">
-                    <img src={user.avatar_url} className="card-img-top" alt="avatar"/>
+            <div className="container user-container">
+                <div className="card card-box">
+                    <img src={user.avatar_url} className="card-img-top card-img" alt="avatar"/>
                     <div className="card-body">
                         <h5 className="card-title">{user.login}</h5>
                         <h6>{user.created_at}</h6>
-                        <p className="card-text">Followers: {user.followers}</p>
-                        <p className="card-text">Following: {user.following}</p>
-                        <p className="card-text">Public repos: {user.public_repos}</p>
-                        <a href={user.html_url} className="btn btn-primary">See profile</a>
+                        <ul className="list-group list-group-flush description-box">
+                            <li className="list-group-item">Followers: {user.followers}</li>
+                            <li className="list-group-item">Following: {user.following}</li>
+                            <li className="list-group-item">Public repos: {user.public_repos}</li>
+                        </ul>
+                        <a href={user.html_url} className="btn btn-block card-btn">See profile</a>
                     </div>
                 </div>
             </div>
