ada0808e9c5d5e4c833d260eedffbf016f2aacc9
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-01 10:43:21 +0200

    Updates

---
+++ precache-manifest.2fa4fdd466c108e8bfa88e5e8bab35a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5154379a217b0ac4574509bdb42dc9",
+    "url": "/github-users-app/index.html"
+  },
+  {
+    "revision": "078f0488eb5ace1b4cde",
+    "url": "/github-users-app/static/css/main.20895ec2.chunk.css"
+  },
+  {
+    "revision": "fa4cb357bacda363319a",
+    "url": "/github-users-app/static/js/2.ed75a3ac.chunk.js"
+  },
+  {
+    "revision": "078f0488eb5ace1b4cde",
+    "url": "/github-users-app/static/js/main.ba6fb692.chunk.js"
+  },
+  {
+    "revision": "3a5536abf365c9a6e412",
+    "url": "/github-users-app/static/js/runtime~main.f99d39a8.js"
+  }
+]);
