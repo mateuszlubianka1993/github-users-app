3ff4494a6e32204d0c17e54618252a72fe4c4872
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-01 10:37:03 +0200

    add target="_blank" property

--- src/components/UserDisplay.js
+++ src/components/UserDisplay.js
@@ -19,7 +19,7 @@ class UserDisplay extends React.Component {
                             <li className="list-group-item">Following: {user.following}</li>
                             <li className="list-group-item">Public repos: {user.public_repos}</li>
                         </ul>
-                        <a href={user.html_url} className="btn btn-block card-btn">See profile</a>
+                        <a href={user.html_url} target="_blank" rel="noopener noreferrer" className="btn btn-block card-btn">See profile</a>
                     </div>
                 </div>
             </div>
